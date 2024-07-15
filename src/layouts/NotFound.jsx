9466a31c6863718600f3ff87abd1c18294990aaf
julio-olivares-porfin <julio@por-fin.cl>
2024-07-15 00:19:06 -0400

    subiendo notfound

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -1,10 +1,23 @@
 import { Container } from "react-bootstrap"
+import { useNavigate } from "react-router-dom";
 import notFoundImg from "../assets/MissingNO.webp"
 
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const irAPokemones = () => {
+    navigate("/pokemones");
+  };
   return (
-    <Container>
+    <Container className="main">
+      <h2>
+        No se encuentra el sitio
+      </h2>
       <img src={notFoundImg} alt="missing no." />
+      <button className="ochobit-button-small" onClick={irAPokemones}>
+        volver al home
+      </button>
     </Container>
   )
 }
